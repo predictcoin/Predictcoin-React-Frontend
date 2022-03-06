7df23e8568a6e08a49c2e204bd8f1c5ea2f29cbe
nonseodion <nonseifebhor@gmail.com>
2022-03-06 15:41:43 +0100

    WIP: added Prediction Contract and Wallet to Application

---
+++ src/views/Dapp/application/infrastructure/redux/reducers/wallet.ts
@@ -0,0 +1,26 @@
+import { WalletStore } from "../../../domain/wallet/walletStore"
+import * as actionTypes from "../actionTypes/wallet";
+
+type INITIAL_STATE = Omit<WalletStore, "connect" | "disconnect" | "explorer" | "name" | "chainId">
+
+const initial: INITIAL_STATE = {
+  wallet: { address: "", provider: undefined},
+  isConnecting: false,
+  active: false,
+}
+
+export const walletReducer = (state: INITIAL_STATE = initial, action: {type: string, data?: any}) => {
+  switch(action.type){
+    case actionTypes.CONNECT_WALLET:
+      return {...state, isConnecting: true};
+    case actionTypes.SET_WALLET:
+      return {...state, ...action.data, isConnecting: false}
+    case actionTypes.CONNECT_WALLET_SUCCESS:
+      return {...state, ...action.data, isConnecting: false, active:true}
+    case actionTypes.DISCONNECT_WALLET:
+      return {...state, isConnecting: false, active: false}
+    default:
+      return state
+  }
+}
+
