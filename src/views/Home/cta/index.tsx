3fd4b3eee2b37722e1ed783e5d80ffb4c3690844
Aydesmond <avalonmsg@gmail.com>
2022-03-31 13:17:35 +0100

    Added crono image on the landing page
    Added crono image on the landing page

--- src/views/Home/cta/index.tsx
+++ src/views/Home/cta/index.tsx
@@ -1,33 +1,38 @@
-import { FC } from 'react';
-import { Link } from 'react-router-dom';
+import { FC } from "react";
+import { Link } from "react-router-dom";
+import crono from "../../../assets/cronos2x.png";
 
 const CTA: FC = () => {
-	return (
-		<section id='cta' className='cta'>
-			<div className='container' data-aos='zoom-in'>
-				<div className='row'>
-					<div className='col-lg-9 text-center text-lg-start'>
-						<h3>
-							Predict crypto-assets, sports games and have fun in the arena
-							while you earn
-						</h3>
-						<p>
+    return (
+        <section id="cta" className="cta">
+            <div className="container" data-aos="zoom-in">
+                <div className="row">
+                    <div className="col-lg-9 text-center text-lg-start">
+                        <h3>
+                            Predict crypto-assets, sports games and have fun in
+                            the arena while you earn
+                        </h3>
+                        {/* <p>
 							{' '}
 							Duis aute irure dolor in reprehenderit in voluptate velit esse
 							cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
 							cupidatat non proident, sunt in culpa qui officia deserunt mollit
 							anim id est laborum.
-						</p>
-					</div>
-					<div className='col-lg-3 cta-btn-container text-center'>
-						<Link className='cta-btn align-middle' to='/staking'>
-							Launch DApp
-						</Link>
-					</div>
-				</div>
-			</div>
-		</section>
-	);
+						</p> */}
+                    </div>
+                    <div className="col-lg-3 cta-btn-container text-center">
+                        <Link className="cta-btn align-middle" to="/staking">
+                            Launch DApp
+                        </Link>
+                    </div>
+
+                    <div className='crono-container'>
+                        <img className="crono-image" src={crono} alt="crono-image" />
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default CTA;
