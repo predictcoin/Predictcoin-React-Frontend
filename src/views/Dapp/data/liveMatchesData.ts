ab7cbf481d0938c4a04d3ad680fbff9c50edb7c0
AjayiMike <ajayimikeofficial@gmail.com>
2022-04-13 19:14:16 +0100

    add live matches fake data and live matches data model

---
+++ src/views/Dapp/data/liveMatchesData.ts
@@ -0,0 +1,106 @@
+import liveMatchModel from "../models/liveMatchModel";
+
+const liveMatchData: liveMatchModel[] = [
+    {
+        id: "1",
+        timeGone: "46:30mins",
+        team_one_name: "Manchester UTD",
+        team_two_name: "Napoli",
+        team_one_score: 4,
+        team_two_score: 2,
+        team_one_logo_uri: "/assets/img/manchester_united_logo.png",
+        team_two_logo_uri: "/assets/img/napoli_logo.png"
+    },
+    {
+        id: "2",
+        timeGone: "46:30mins",
+        team_one_name: "Manchester UTD",
+        team_two_name: "Napoli",
+        team_one_score: 4,
+        team_two_score: 2,
+        team_one_logo_uri: "/assets/img/manchester_united_logo.png",
+        team_two_logo_uri: "/assets/img/napoli_logo.png"
+    },
+    {
+        id: "3",
+        timeGone: "46:30mins",
+        team_one_name: "Manchester UTD",
+        team_two_name: "Napoli",
+        team_one_score: 4,
+        team_two_score: 2,
+        team_one_logo_uri: "/assets/img/manchester_united_logo.png",
+        team_two_logo_uri: "/assets/img/napoli_logo.png"
+    },
+    {
+        id: "4",
+        timeGone: "46:30mins",
+        team_one_name: "Manchester UTD",
+        team_two_name: "Napoli",
+        team_one_score: 4,
+        team_two_score: 2,
+        team_one_logo_uri: "/assets/img/manchester_united_logo.png",
+        team_two_logo_uri: "/assets/img/napoli_logo.png"
+    },
+    {
+        id: "5",
+        timeGone: "46:30mins",
+        team_one_name: "Manchester UTD",
+        team_two_name: "Napoli",
+        team_one_score: 4,
+        team_two_score: 2,
+        team_one_logo_uri: "/assets/img/manchester_united_logo.png",
+        team_two_logo_uri: "/assets/img/napoli_logo.png"
+    },
+    {
+        id: "6",
+        timeGone: "46:30mins",
+        team_one_name: "Manchester UTD",
+        team_two_name: "Napoli",
+        team_one_score: 4,
+        team_two_score: 2,
+        team_one_logo_uri: "/assets/img/manchester_united_logo.png",
+        team_two_logo_uri: "/assets/img/napoli_logo.png"
+    },
+    {
+        id: "7",
+        timeGone: "46:30mins",
+        team_one_name: "Manchester UTD",
+        team_two_name: "Napoli",
+        team_one_score: 4,
+        team_two_score: 2,
+        team_one_logo_uri: "/assets/img/manchester_united_logo.png",
+        team_two_logo_uri: "/assets/img/napoli_logo.png"
+    }
+    ,{
+        id: "8",
+        timeGone: "46:30mins",
+        team_one_name: "Manchester UTD",
+        team_two_name: "Napoli",
+        team_one_score: 4,
+        team_two_score: 2,
+        team_one_logo_uri: "/assets/img/manchester_united_logo.png",
+        team_two_logo_uri: "/assets/img/napoli_logo.png"
+    },
+    {
+        id: "9",
+        timeGone: "46:30mins",
+        team_one_name: "Manchester UTD",
+        team_two_name: "Napoli",
+        team_one_score: 4,
+        team_two_score: 2,
+        team_one_logo_uri: "/assets/img/manchester_united_logo.png",
+        team_two_logo_uri: "/assets/img/napoli_logo.png"
+    },
+    {
+        id: "10",
+        timeGone: "46:30mins",
+        team_one_name: "Manchester UTD",
+        team_two_name: "Napoli",
+        team_one_score: 4,
+        team_two_score: 2,
+        team_one_logo_uri: "/assets/img/manchester_united_logo.png",
+        team_two_logo_uri: "/assets/img/napoli_logo.png"
+    }
+]
+
+export default liveMatchData;
---
+++ src/views/Dapp/models/liveMatchModel.ts
@@ -0,0 +1,12 @@
+interface liveMatchModel {
+    id: string;
+    timeGone: string;
+    team_one_name: string;
+    team_two_name: string;
+    team_one_score: number
+    team_two_score: number
+    team_one_logo_uri: string;
+    team_two_logo_uri: string;
+}
+
+export default liveMatchModel;
