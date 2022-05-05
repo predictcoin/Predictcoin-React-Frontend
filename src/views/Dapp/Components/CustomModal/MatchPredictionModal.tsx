db796f3bc4df8bd50a8481244142e01741b87424
AjayiMike <ajayimikeofficial@gmail.com>
2022-05-05 20:39:02 +0100

    change match prediction modal message

--- src/views/Dapp/Components/CustomModal/MatchPredictionModal.tsx
+++ src/views/Dapp/Components/CustomModal/MatchPredictionModal.tsx
@@ -14,31 +14,44 @@ import { SPORT_PREDICTION_ADDRESSES, TOKENS } from "../../constants/addresses";
 import { useWalletViewModel } from "../../application/controllers/walletViewModel";
 import { ethers } from "ethers";
 
-
-
 const MatchPredictionModal: FC = () => {
-
-    const {predict, predictMatchModal, upcomingMatches, rewardMultiplier, predictionAmount} = useSportPredictionViewModel()
+    const {
+        predict,
+        predictMatchModal,
+        upcomingMatches,
+        rewardMultiplier,
+        predictionAmount
+    } = useSportPredictionViewModel();
     const { active, chainId } = useWalletViewModel();
-    const {allowances, getAllowance, approve} = useToken(TOKENS[chainId].CRP)
+    const { allowances, getAllowance, approve } = useToken(TOKENS[chainId].CRP);
 
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
     const pendingToast = useRef("" as ReactText);
 
     const closeModal = () => {
-        setPredictMatchModal(null)(dispatch)
-    }
+        setPredictMatchModal(null)(dispatch);
+    };
     const closeModalFunc = (e: any) => {
         if (e.target?.id === "match__prediction__modal") closeModal();
     };
-    const [match, setMatch] = useState<UpcomingMatch>()
-    const [predictionData, setPredictionData] = useState<{teamA: string, teamB: string}>({teamA: "", teamB: ""})
+    const [match, setMatch] = useState<UpcomingMatch>();
+    const [predictionData, setPredictionData] = useState<{
+        teamA: string;
+        teamB: string;
+    }>({ teamA: "", teamB: "" });
 
     useEffect(() => {
-        const targetMatch = upcomingMatches.find(match => match.id === predictMatchModal.id)
-        setMatch(targetMatch)
+        const targetMatch = upcomingMatches.find(
+            (match) => match.id === predictMatchModal.id
+        );
+        setMatch(targetMatch);
 
-        getAllowance(SPORT_PREDICTION_ADDRESSES[process.env.REACT_APP_ENVIRONMENT as keyof typeof SPORT_PREDICTION_ADDRESSES])
+        getAllowance(
+            SPORT_PREDICTION_ADDRESSES[
+                process.env
+                    .REACT_APP_ENVIRONMENT as keyof typeof SPORT_PREDICTION_ADDRESSES
+            ]
+        );
         window.addEventListener("click", (e) => closeModalFunc(e));
 
         return () => {
@@ -48,29 +61,56 @@ const MatchPredictionModal: FC = () => {
     }, []);
 
     const handleOnchange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        if(!/^[0-9]\d*$/.test(event.target.value) && event.target.value !== "") return event.preventDefault()
-        if(Number(event.target.value) > 100) return event.preventDefault()
-        return setPredictionData(prev => ({...prev, [event.target.name]: event.target.value})) 
-    } 
+        if (!/^[0-9]\d*$/.test(event.target.value) && event.target.value !== "")
+            return event.preventDefault();
+        if (Number(event.target.value) > 100) return event.preventDefault();
+        return setPredictionData((prev) => ({
+            ...prev,
+            [event.target.name]: event.target.value
+        }));
+    };
 
-    const handlePredict = async (event: React.MouseEvent<HTMLElement>) => {        
+    const handlePredict = async (event: React.MouseEvent<HTMLElement>) => {
         event.preventDefault();
-        if(!active) {
-            const body = ToastBody("You are not connected. Please connect your wallet to place predicition", STATUS.ERROR, TYPE.ERROR)
+        if (!active) {
+            const body = ToastBody(
+                "You are not connected. Please connect your wallet to place predicition",
+                STATUS.ERROR,
+                TYPE.ERROR
+            );
             toast.dismiss(pendingToast.current);
             return toast(body);
         }
-        if(!predictionData.teamA || !predictionData.teamB) {
-            const body = ToastBody("Please enter your score prediction for both teams", STATUS.ERROR, TYPE.ERROR)
+        if (!predictionData.teamA || !predictionData.teamB) {
+            const body = ToastBody(
+                "Please enter your score prediction for both teams",
+                STATUS.ERROR,
+                TYPE.ERROR
+            );
             toast.dismiss(pendingToast.current);
             return toast(body);
         }
 
         try {
-            if(predictionAmount.gt(allowances[SPORT_PREDICTION_ADDRESSES[process.env.REACT_APP_ENVIRONMENT as keyof typeof SPORT_PREDICTION_ADDRESSES]])) {
-                await approve(SPORT_PREDICTION_ADDRESSES[process.env.REACT_APP_ENVIRONMENT as keyof typeof SPORT_PREDICTION_ADDRESSES], ethers.constants.MaxUint256)
+            if (
+                predictionAmount.gt(
+                    allowances[
+                        SPORT_PREDICTION_ADDRESSES[
+                            process.env
+                                .REACT_APP_ENVIRONMENT as keyof typeof SPORT_PREDICTION_ADDRESSES
+                        ]
+                    ]
+                )
+            ) {
+                await approve(
+                    SPORT_PREDICTION_ADDRESSES[
+                        process.env
+                            .REACT_APP_ENVIRONMENT as keyof typeof SPORT_PREDICTION_ADDRESSES
+                    ],
+                    ethers.constants.MaxUint256
+                );
             }
-    
+
             const callbacks = {
                 successfull: () => {
                     closeModal();
@@ -78,15 +118,21 @@ const MatchPredictionModal: FC = () => {
                 failed: () => {
                     closeModal();
                 }
-            }
-            
-            predict(match?.id as string, match?.teamA as string, match?.teamB as string, Number(predictionData.teamA), Number(predictionData.teamB), callbacks)
-        }catch(err) {
-            console.error(err)
+            };
+
+            predict(
+                match?.id as string,
+                match?.teamA as string,
+                match?.teamB as string,
+                Number(predictionData.teamA),
+                Number(predictionData.teamB),
+                callbacks
+            );
+        } catch (err) {
+            console.error(err);
             closeModal();
         }
-        
-    }
+    };
 
     return (
         <div id="match__prediction__modal">
@@ -97,16 +143,17 @@ const MatchPredictionModal: FC = () => {
 
                 <div className="modal__head">
                     <h2>MATCH PREDICTIONS</h2>
-                    <p>
-                        {`You will be charged ${formatEther(predictionAmount)} CRP for each pool entered and
-                        earn X${rewardMultiplier} (${Number(formatEther(predictionAmount)) * rewardMultiplier} CRO) when you win.`}
-                    </p>
+                    <p>{`Predict the score at the end of regular time. You will be charged ${formatEther(
+                        predictionAmount
+                    )} CRP for each pool entered and earn X${rewardMultiplier} (${
+                        Number(formatEther(predictionAmount)) * rewardMultiplier
+                    } CRP) when you win.`}</p>
 
                     <div className="modal__body">
                         <div className="team__details">
                             <div className="team__a">
                                 <img
-                                    src= {match?.teamALogoUri}
+                                    src={match?.teamALogoUri}
                                     alt={match?.teamA + " logo"}
                                 />
                                 <p>{match?.teamA}</p>
@@ -114,7 +161,7 @@ const MatchPredictionModal: FC = () => {
                             <GoDash className="seperator" />
                             <div className="team__b">
                                 <img
-                                    src= {match?.teamBLogoUri}
+                                    src={match?.teamBLogoUri}
                                     alt={match?.teamB + " logo"}
                                 />
                                 <p>{match?.teamB}</p>
@@ -128,7 +175,7 @@ const MatchPredictionModal: FC = () => {
                                         name="teamA"
                                         placeholder="0"
                                         value={predictionData.teamA}
-                                        onChange = {handleOnchange}
+                                        onChange={handleOnchange}
                                     />
                                 </label>
                                 <p className="text">
@@ -140,11 +187,13 @@ const MatchPredictionModal: FC = () => {
                                         name="teamB"
                                         placeholder="0"
                                         value={predictionData.teamB}
-                                        onChange = {handleOnchange}
+                                        onChange={handleOnchange}
                                     />
                                 </label>
                             </div>
-                            <button onClick = {handlePredict}>Predict scores</button>
+                            <button onClick={handlePredict}>
+                                Predict scores
+                            </button>
                         </form>
                     </div>
                 </div>
