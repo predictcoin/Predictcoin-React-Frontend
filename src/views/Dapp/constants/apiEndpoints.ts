26a9d8a816c6b1c81fe8adc335558838d6a039d6
nonseodion <nonseifebhor@gmail.com>
2022-05-09 15:47:20 +0100

    updated api endpoint

--- src/views/Dapp/constants/apiEndpoints.ts
+++ src/views/Dapp/constants/apiEndpoints.ts
@@ -1,2 +1,2 @@
-export const sportApiEndpoint = "https://predictcoin-mainnet-staging.herokuapp.com/football-api";
+export const sportApiEndpoint = "https://predictcoin-production.herokuapp.com/football-api";
 export const worldTimeApiEndpoint = "https://worldtimeapi.org/api/timezone/Africa/Lagos"
