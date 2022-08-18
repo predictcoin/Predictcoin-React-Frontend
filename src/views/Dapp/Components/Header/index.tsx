c139e8303e55bcca974bc864a3f8bc4584750187
AjayiMike <ajayimikeofficial@gmail.com>
2022-08-18 21:36:19 +0100

    use a clearer predcoin logo image

--- src/views/Dapp/Components/Header/index.tsx
+++ src/views/Dapp/Components/Header/index.tsx
@@ -10,54 +10,78 @@ import { displayDecimals } from "../../lib/utils/number";
 import TxModal from "../CustomModal/TransactionModal";
 import "./header.styles.scss";
 
-
 interface HeaderProps {
-  title: string,
-  subtitle: string,
-  setIsSidebarExpanded: Dispatch<React.SetStateAction<boolean>>
-  isSidebarExpanded: boolean
-  setModalOpened: Dispatch<React.SetStateAction<boolean>>
+    title: string;
+    subtitle: string;
+    setIsSidebarExpanded: Dispatch<React.SetStateAction<boolean>>;
+    isSidebarExpanded: boolean;
+    setModalOpened: Dispatch<React.SetStateAction<boolean>>;
 }
-const Header: FC<HeaderProps> = ({subtitle, title, setIsSidebarExpanded, isSidebarExpanded, setModalOpened}) => {
-  const {balance, decimals} = useToken(TOKENS[getChainId()].PRED)
-  const {address, active} = useWalletViewModel();
-  const [openTxs, setOpenTxs] = useState(false);
+const Header: FC<HeaderProps> = ({
+    subtitle,
+    title,
+    setIsSidebarExpanded,
+    isSidebarExpanded,
+    setModalOpened
+}) => {
+    const { balance, decimals } = useToken(TOKENS[getChainId()].PRED);
+    const { address, active } = useWalletViewModel();
+    const [openTxs, setOpenTxs] = useState(false);
 
-  return (
-    <header>
-      {openTxs && <TxModal closeModal={setOpenTxs}/>}
-      <div className="top-header">
-        <button
-          className='hamburger'
-          onClick={() =>
-            setIsSidebarExpanded((isSidebarExpanded) => !isSidebarExpanded)
-          }
-        >
-          <div id='nav-icon1' className={!isSidebarExpanded ? 'open' : ''}>
-            <span></span>
-            <span></span>
-            <span></span>
-          </div>
-        </button>
-        <div className='header__text header__text--1'>
-          <h1>{title}</h1>
-          <p>{subtitle}</p>
-        </div>
-        <div className='header__ctas'>
-          {
-            active && 
-            <div className='wallet__price'>
-              <img src="/assets/img/predcoin_logo.png" alt='predict-coin-logo' />
-              <p>{displayDecimals(ethers.utils.formatUnits(balance, decimals), 5)}</p>
-            </div>
-          } 
-          &nbsp; &nbsp;
-          {/* add 'not__connected class if wallet is not connected' */}
-          <button className={`address ${active || "not__connected"}`} onClick={() => setModalOpened(true)}>
-              <WalletIcon />
-            <span>{active ? shortenAddress(address || ""): "connect wallet"}</span>
-          </button>
-          {/* &nbsp; &nbsp;
+    return (
+        <header>
+            {openTxs && <TxModal closeModal={setOpenTxs} />}
+            <div className="top-header">
+                <button
+                    className="hamburger"
+                    onClick={() =>
+                        setIsSidebarExpanded(
+                            (isSidebarExpanded) => !isSidebarExpanded
+                        )
+                    }
+                >
+                    <div
+                        id="nav-icon1"
+                        className={!isSidebarExpanded ? "open" : ""}
+                    >
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </button>
+                <div className="header__text header__text--1">
+                    <h1>{title}</h1>
+                    <p>{subtitle}</p>
+                </div>
+                <div className="header__ctas">
+                    {active && (
+                        <div className="wallet__price">
+                            <img
+                                src="/assets/img/predcoin_logo.png"
+                                alt="predict-coin-logo"
+                            />
+                            <p>
+                                {displayDecimals(
+                                    ethers.utils.formatUnits(balance, decimals),
+                                    5
+                                )}
+                            </p>
+                        </div>
+                    )}
+                    &nbsp; &nbsp;
+                    {/* add 'not__connected class if wallet is not connected' */}
+                    <button
+                        className={`address ${active || "not__connected"}`}
+                        onClick={() => setModalOpened(true)}
+                    >
+                        <WalletIcon />
+                        <span>
+                            {active
+                                ? shortenAddress(address || "")
+                                : "connect wallet"}
+                        </span>
+                    </button>
+                    {/* &nbsp; &nbsp;
           {
             active && 
             <button 
@@ -69,15 +93,14 @@ const Header: FC<HeaderProps> = ({subtitle, title, setIsSidebarExpanded, isSideb
               <span>3</span>
             </button>
           } */}
-        </div>
-      </div>
-        <div className='header__text header__text--2'>
-          <h1>{title}</h1>
-          <p>{subtitle}</p>
-        </div>
-      
-    </header>
-  )
-}
+                </div>
+            </div>
+            <div className="header__text header__text--2">
+                <h1>{title}</h1>
+                <p>{subtitle}</p>
+            </div>
+        </header>
+    );
+};
 
 export default Header;
